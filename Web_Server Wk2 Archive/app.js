e1a093e79c99a39640b32d787fb11631cc23ea60
Marjana Cowan <mdcowan@student.fullsail.edu>
2020-03-15 20:36:37 -0400

    Wk3 Lecture NPM, Express, NodeMon incomplete

---
+++ Web_Server Wk2 Archive/app.js
@@ -0,0 +1,53 @@
+var fs = require("fs")
+var http = require("http")
+var path = require("path")
+var url = require("url")
+
+http.createServer(function(request,response){
+
+    /*get the request URL from the browser then use the URL library
+    to chop that into pieces and create an object so now the parse 
+    contains an object with the information that is gathered from the URL
+    */
+    var parsed = url.parse(request.url)
+    //now we can use parse from the path library for the parsed result
+    var filename = path.parse(parsed.pathname)
+
+    //save the file parts for use from the parse
+    var filen = filename.name==""?"index":filename.name
+    var ext = filename.ext==""?".html":filename.ext
+    var dir = filename.dir=="/"?"":filename.dir+"/"
+    var page = filename.name==""?"index":filename.name
+
+    //create the path with file name and ext
+    f=(dir+filen+ext).replace("/","")
+
+    var mimeTypes = {
+        '.html' : 'text/html',
+        '.js' : 'text/javascript',
+        '.css' : 'text/css',
+        '.png' : 'image/png',
+        '.jpg' : 'image/jpg',
+        '.gif' : 'image/gif'
+    }
+
+    if(f){
+        fs.readFile(f,function(err, data){
+
+            if(page){
+                if(mimeTypes.hasOwnProperty(ext)){
+                    response.writeHead(200, {'Content-Type' : mimeTypes[ext]}) //header()
+                    //this is how to pass data to the HTML page.
+                    //a variable is created with data in it.
+                    //the variable is then called in the HTML page JavaScript
+                    response.write("<script>var page = '"+filen+"'</script>")
+                    response.end(data, 'utf-8')
+                }
+            }
+    
+        })
+    }
+
+}).listen("8080", function(){
+    console.log("info", 'Server is at port: ' + 8080)
+})
--- Web_Server/app.js
+++ Web_Server/app.js
@@ -1,53 +1,52 @@
-var fs = require("fs")
-var http = require("http")
-var path = require("path")
-var url = require("url")
+//Enable JS Strict Mode
+//see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+"use strict"
 
-http.createServer(function(request,response){
+var fs = require("fs") //https://nodejs.dev/the-nodejs-fs-module
+var http = require("http") //https://nodejs.org/api/http.html
+var path = require("path") //https://nodejs.org/api/path.html
+var url = require("url") //https://nodejs.org/api/url.html
 
-    /*get the request URL from the browser then use the URL library
-    to chop that into pieces and create an object so now the parse 
-    contains an object with the information that is gathered from the URL
-    */
-    var parsed = url.parse(request.url)
-    //now we can use parse from the path library for the parsed result
-    var filename = path.parse(parsed.pathname)
+//install JS libraries
+// sudo npm install -g express
+// https://expressjs.com/
+// sudo npm install -g ejs
+// https://www.npmjs.com/package/ejs
+// sudo npm install -g request 
+// https://github.com/request/request
+// sudo npm install -g body-parser
+// https://www.npmjs.com/package/body-parser
 
-    //save the file parts for use from the parse
-    var filen = filename.name==""?"index":filename.name
-    var ext = filename.ext==""?".html":filename.ext
-    var dir = filename.dir=="/"?"":filename.dir+"/"
-    var page = filename.name==""?"index":filename.name
+// import JS libraries
+var express = require("express") 
+var request = require("request")
+var bodyParser = require("body-parser") 
 
-    //create the path with file name and ext
-    f=(dir+filen+ext).replace("/","")
 
-    var mimeTypes = {
-        '.html' : 'text/html',
-        '.js' : 'text/javascript',
-        '.css' : 'text/css',
-        '.png' : 'image/png',
-        '.jpg' : 'image/jpg',
-        '.gif' : 'image/gif'
-    }
+let ejs = require("ejs")
+const router = express.Router()
+var app = express()
+app.set("view engine","ejs")
+app.engine("ejs",require("ejs").__express)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
-    if(f){
-        fs.readFile(f,function(err, data){
-
-            if(page){
-                if(mimeTypes.hasOwnProperty(ext)){
-                    response.writeHead(200, {'Content-Type' : mimeTypes[ext]}) //header()
-                    //this is how to pass data to the HTML page.
-                    //a variable is created with data in it.
-                    //the variable is then called in the HTML page JavaScript
-                    response.write("<script>var page = '"+filen+"'</script>")
-                    response.end(data, 'utf-8')
-                }
-            }
-    
-        })
-    }
-
-}).listen("8080", function(){
-    console.log("info", 'Server is at port: ' + 8080)
+router.get("/", function(req,res){
+    //note that we are no longer using file write.
+    //here we are pasing data to the file - variable pagename = Home
+    res.render("index", {pagename:"Home"}) //views/index.ejs
 })
+router.get("/about", function(req,res){
+    //note that we are no longer using file write.
+    //here we are pasing data to the file - variable pagename = About
+    res.render("about", {pagename:"About"}) //views/about.ejs
+})
+
+
+
+app.use(express.static("public"))
+app.use("/",router)
+var server = app.listen("8080")
+
+
+
