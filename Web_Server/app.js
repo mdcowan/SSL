8fb0a9d63a826c7d567d98bb29da2b533780864e
Marjana Cowan <mdcowan@student.fullsail.edu>
2020-03-16 20:07:03 -0400

    Week 3 Lecture 32:21 Code Incomplete

--- Web_Server/app.js
+++ Web_Server/app.js
@@ -30,22 +30,45 @@ app.set("view engine","ejs")
 app.engine("ejs",require("ejs").__express)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use("/",router)
 
 router.get("/", function(req,res){
     //note that we are no longer using file write.
     //here we are pasing data to the file - variable pagename = Home
-    res.render("index", {pagename:"Home"}) //views/index.ejs
+    res.render("index", {pagename:"home"}) //views/index.ejs
 })
 router.get("/about", function(req,res){
     //note that we are no longer using file write.
     //here we are pasing data to the file - variable pagename = About
-    res.render("about", {pagename:"About"}) //views/about.ejs
+    res.render("about", {pagename:"about"}) //views/about.ejs
+})
+
+//Because this route is "post", its only purpose is to pass data
+//from the page to the function here.
+router.post("/login", function(req,res){
+    //console.log(req.body)
+    //console.log(req.body.email)
+    //console.log(req.body.password)
+
+    var errors = []
+    if(req.body.email == ""){
+        errors.push("Email is required")
+    }
+    if(req.body.password == ""){
+        errors.push("Password is required")
+    }
+    if(!/^[a-zA-Z]\w{3,14}$/.test(req.body.email)){
+        errors.push("Email is not valid")
+    }
+    if(!/^[a-zA-Z]\w{3,14}$/.test(req.body.password)){
+        errors.push("Password is not valid")
+    }
+
+    res.render("index", {pagename:"home", errors:errors})
 })
 
 
-
-app.use(express.static("public"))
-app.use("/",router)
 var server = app.listen("8080")
 
 
